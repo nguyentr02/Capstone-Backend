30f45f81f86dd353a9545ef67ba40c2cf2bd01a3
minhphan6624 <quangminh6624@gmail.com>
2024-10-13 16:26:59 +1100

    Set up register route and controller

--- models/userModel.js
+++ models/userModel.js
@@ -8,9 +8,28 @@ const User = db.define('User', {
     last_name: { type: DataTypes.STRING },
     email: { type: DataTypes.STRING, unique: true, allowNull: false },
     phone_no: { type: DataTypes.STRING },
-    role: { type: DataTypes.STRING },
-    password: { type: DataTypes.STRING, allowNull: false },
+    role: {
+        type: DataTypes.ENUM('participant', 'organizer', 'admin'),
+        defaultValue: 'participant',
+    },
+    password_hash: { type: DataTypes.STRING, allowNull: false },
     created_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW }
-});
+},
+    {
+        hooks:
+        // Hash the password before saving the user
+        {
+            beforeCreate: async (user) => {
+                const salt = await bcrypt.genSalt(10);
+                user.password_hash = await bcrypt.hash(user.password_hash, salt);
+            },
+            beforeUpdate: async (user) => {
+                if (user.changed('password_hash')) {
+                    const salt = await bcrypt.genSalt(10);
+                    user.password_hash = await bcrypt.hash(user.password_hash, salt);
+                }
+            }
+        }
+    });
 
 module.exports = User;
